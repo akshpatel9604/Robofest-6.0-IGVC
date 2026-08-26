99bd032c43f80e4956a73085758cc61b19618d3d
akshpatel9604 <akshpatel.9604@gmail.com>
2026-08-26 14:53:02 +0530

    Update gpio_on_off_demo.py

--- software/motors/gpio_on_off_demo.py
+++ software/motors/gpio_on_off_demo.py
@@ -1,1 +1,13 @@
+from gpiozero import DigitalOutputDevice
+from time import sleep
 
+out = DigitalOutputDevice(16)  # output pin
+
+try:
+    while True:
+        out.on()      # HIGH
+        sleep(2)
+        out.off()     # LOW
+        sleep(2)
+except KeyboardInterrupt:
+    out.close()
