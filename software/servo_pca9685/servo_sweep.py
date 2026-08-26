b04bd0b81eb628f67b7e19939e0f6ff0f1f9167e
akshpatel9604 <akshpatel.9604@gmail.com>
2026-08-26 14:56:25 +0530

    Update servo_sweep.py

--- software/servo_pca9685/servo_sweep.py
+++ software/servo_pca9685/servo_sweep.py
@@ -1,1 +1,14 @@
+from adafruit_servokit import ServoKit
+import time
 
+pca = ServoKit(channels=16)
+
+pca.servo[0].angle = 0
+
+for i in range(100):
+    pca.servo[0].angle = i
+    time.sleep(0.1)
+
+for i in range(180, 0, -1):
+    pca.servo[0].angle = i
+    time.sleep(0.1)
