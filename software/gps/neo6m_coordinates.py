4811bf44f6ae7594dd3f7a504650fa2d26ec5081
akshpatel9604 <akshpatel.9604@gmail.com>
2026-08-26 14:49:37 +0530

    Update neo6m_coordinates.py

--- software/gps/neo6m_coordinates.py
+++ software/gps/neo6m_coordinates.py
@@ -1,1 +1,13 @@
+import serial
+import pynmea2
 
+port = "/dev/serial0"
+s = serial.Serial(port, baudrate=9600, timeout=0.8)
+
+while True:
+    data = s.readline().decode("utf-8", errors="ignore")
+    if data.startswith("$GPGLL") or data.startswith("$GNGLL"):
+        location = pynmea2.parse(data)
+        latitude = location.latitude
+        longitude = location.longitude
+        print("Latitude:", latitude, "\tLongitude:", longitude)
